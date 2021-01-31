662e3a73fde5dfe7ccc93f5b24234cabd5f27e9e
regnents <rafaelseanputra@gmail.com>
2021-01-31 20:45:31 +0700

    first commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Titik asal lintasan bola saat ini
+    private Vector2 trajectoryOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        // Mulai game
+        RestartGame();
+        trajectoryOrigin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        float xForce = xInitialForce;
+        float yForce = yInitialForce;
+        
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomX = Random.Range(0, 2);
+
+        float randomY = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomX < 1.0f)
+        {
+
+            xForce = xForce * (-1);
+        }
+
+        if (randomY < 1.0f)
+        {
+            yForce = yForce * (-1);
+        }
+
+        // Gunakan gaya untuk menggerakkan bola ini.
+        rigidBody2D.AddForce(new Vector2(xForce, yForce));
+    }
+
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
